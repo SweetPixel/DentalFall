1be929e32e24833b0311df148584857aebcd7d66
ebtihaj-khan <ebtihaj_316@yahoo.com>
2014-04-13 19:56:20 +0500

    Basic functionality added
    All the basic functionality has been added. Just one bug remains that is
    of spawning of enemies.

---
+++ Pixel.Fall/Assets/enemy fall.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var enemyPrefab : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+var forward=transform.TransformDirection(Vector3.forward);
+var enemy= Instantiate(enemyPrefab, GameObject.Find("Cube").transform.position, Quaternion.identity);
+//enemy.rigidbody.AddForce(transform.forward);
+}
---
+++ Pixel.Fall/Assets/move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(MeshCollider))]
+
+public class move : MonoBehaviour {
+	private Vector3 screenpoint;
+	 int i=5;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity=Vector3.down*i;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown()
+	{
+		screenpoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+			new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenpoint.z));
+	}
+	
+	
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenpoint.z);
+		
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+}
