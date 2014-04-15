785e77294cbd70b086458cb5080b3005bde36f26
ebtihaj-khan <ebtihaj_316@yahoo.com>
2014-04-15 05:02:00 +0500

    Score and enemy random generation added
    Few of the bugs have been removed and the enemy fall is completely
    implemented to spawn at random positions. And as soon as a enemy strikes
    the floor 1 point will be added to the score. Some unnecessary files
    have been deleted. Do try this and give us the feedback about any bugs.

---
+++ Pixel.Fall/Assets/enemyFall.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+	// Array of enemy prefabs.
+	
+var prefab : Transform;	
+var timer: float=0.0;
+function Start () {
+
+}
+
+function Update () {
+timer+=Time.deltaTime;
+if (timer > 1){
+Spawn();}
+}
+function Spawn(){
+var pos:Vector3 = new Vector3(Random.Range(-10.0F,10.0F),7.709583,-3.021553F);
+timer=0;
+								var forward=transform.TransformDirection(Vector3.down);
+				Instantiate(prefab,pos,Quaternion.identity);
+				
+}
