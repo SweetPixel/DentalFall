61c0e1ae8494b4525af30f442af6b4d83bd252ad
ebtihaj-khan <ebtihaj_316@yahoo.com>
2014-04-15 05:02:00 +0500

    Score and enemy random generation added
    Few of the bugs have been removed and the enemy fall is completely
    implemented to spawn at random positions. And as soon as a enemy strikes
    the floor 1 point will be added to the score. Some unnecessary files
    have been deleted. Do try this and give us the feedback about any bugs.

--- Pixel.Fall/Assets/destroy enemy.js
+++ Pixel.Fall/Assets/destroy enemy.js
@@ -3,14 +3,16 @@
 function Start () {
 
 }
-
+var score:int=0;
+var scoretext: GUIText;
 function Update () {
-
+scoretext.text= "SCORE: "+ score;
 }
 function OnCollisionEnter(col : Collision)
 {
 if(col.gameObject.name=="enemy(Clone)")
 {
 Destroy(col.gameObject);
+score++;
 }
 }
