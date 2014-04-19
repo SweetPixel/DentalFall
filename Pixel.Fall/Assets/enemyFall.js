3fe48bb11dbf24cbdbad4fa316125fe86b226d5e
zahid--ali <zfz.zahid@yahoo.com>
2014-04-19 15:43:18 +0500

    Final cleaned code no smell

--- Pixel.Fall/Assets/enemyFall.js
+++ Pixel.Fall/Assets/enemyFall.js
@@ -2,21 +2,36 @@
 
 	// Array of enemy prefabs.
 	
-var prefab : Transform;	
+var enemy1 : Transform;	
+var enemy2 : Transform;	
+var enemy3 : Transform;	
 var timer: float=0.0;
+
 function Start () {
 
 }
 
 function Update () {
 timer+=Time.deltaTime;
-if (timer > 1){
+if (timer > 0.09){
 Spawn();}
 }
 function Spawn(){
+var randomPick : int = Mathf.Abs(Random.Range(0,4));
 var pos:Vector3 = new Vector3(Random.Range(-10.0F,10.0F),7.709583,-3.021553F);
 timer=0;
-								var forward=transform.TransformDirection(Vector3.down);
-				Instantiate(prefab,pos,Quaternion.identity);
+if(randomPick == 1){
+
+//var forward=transform.TransformDirection(Vector3.down);
+				Instantiate(enemy1,pos,Quaternion.identity);
+}
+else if(randomPick == 2){
+//var forward=transform.TransformDirection(Vector3.down);
+				Instantiate(enemy2,pos,Quaternion.identity);
+}
+else if(randomPick == 3){
+//var forward=transform.TransformDirection(Vector3.down);
+				Instantiate(enemy3,pos,Quaternion.identity);
+}								
 				
 }
