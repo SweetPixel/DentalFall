b975d71b003e6247980ba9fb9956e10ea92ceb88
aliirz <dejavu87@gmail.com>
2014-04-23 17:55:32 +0500

    Fixed MOAR merged

--- Pixel.Fall/Assets/destroy.js
+++ Pixel.Fall/Assets/destroy.js
@@ -9,11 +9,7 @@ function Update () {
 }
 function OnCollisionEnter(col : Collision)
 {
-<<<<<<< HEAD
 if(col.gameObject.name=="ishape"||col.gameObject.name=="ishape(Clone)"||col.gameObject.name=="squareshape(Clone)"||col.gameObject.name=="squareshape" ||col.gameObject.name=="Lshape(Clone)"||col.gameObject.name=="Lshape"||col.gameObject.name=="deadshape(Clone)"||col.gameObject.name=="deadshape"||col.gameObject.name=="floor" )
-=======
-if(col.gameObject.name=="boundry" || col.gameObject.name=="ishape"||col.gameObject.name=="ishape(Clone)"||col.gameObject.name=="squareshape(Clone)"||col.gameObject.name=="squareshape" ||col.gameObject.name=="Lshape(Clone)"||col.gameObject.name=="Lshape"||col.gameObject.name=="deadshape(Clone)"||col.gameObject.name=="deadshape"||col.gameObject.name=="floor" )
->>>>>>> 01469f933bf6684d32f3eb087c746061d51785f5
 {
 Application.LoadLevel("gameover");
 }
