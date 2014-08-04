b4fdc3298f1093db33d2e2d91128f05b7b3f4b0a
zahid--ali <zfz.zahid@yahoo.com>
2014-08-04 15:32:06 +0500

    Algorithm Changes
    -initial speed changed by 0.1 multiplying factor
    -and ceiling score changed by 30

--- Pixel.Fall/Assets/rigidbodyspeed.js
+++ Pixel.Fall/Assets/rigidbodyspeed.js
@@ -3,15 +3,19 @@
 var score : int;
 var speed : float;
 function Start () {
-rigidbody.AddForce(Vector3.down * 0.2);
+rigidbody.AddForce(Vector3.down * 0.1);
 }
 
 function Update () {
 score =  PlayerPrefs.GetInt("PlayerScore");
-if((score>0) && (score<50)){
-speed = score / 1.4;
+if((score>0) && (score<30)){
+speed = score / 1.6;
+Debug.Log(speed + "speed at  sscore = " + score);
 rigidbody.AddForce(Vector3.down * speed);
 
 }
-
+if(score > 30){
+Debug.Log(score + "speed = " + Vector3.down * 18.125);
+rigidbody.AddForce(Vector3.down * 18.125);
+}
 }
