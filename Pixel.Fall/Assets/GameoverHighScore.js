904eeb31d67e307f1050d20f188d445effdd38f0
ebtihaj-khan <ebtihaj_316@yahoo.com>
2014-08-11 01:08:40 +0500

    latest buILD
    facebook invite and share screenshot added, quit functionality added

--- Pixel.Fall/Assets/GameoverHighScore.js
+++ Pixel.Fall/Assets/GameoverHighScore.js
@@ -2,6 +2,8 @@
 var HighScore : GUIText;
 var nowScore : GUIText;
 var explosion : AudioClip;
+var count : int = 0;
+var skin : GUISkin;
 function Start () {
 nowScore.fontSize=40;
 
@@ -47,5 +49,25 @@ audio.Play();
 }
 
 function Update () {
-
+if (Input.GetKeyDown(KeyCode.Escape)) {
+			count=1;
+	}
 }
+function OnGUI(){
+if(count == 1){
+GUI.skin = skin;
+
+
+   GUI.Box(new Rect(0,0,Screen.width,Screen.height),"Exit");
+   GUI.Label(new Rect(Screen.width*1/4,Screen.height*2/6,Screen.width*2/4,Screen.height*1/6), "Are you sure you want to exit the game?");
+if(GUI.Button(Rect(Screen.width/4,Screen.height*3/8,Screen.width/2,Screen.height/8),"Yes"))
+          {
+
+         Application.Quit();
+
+          }
+          if(GUI.Button(Rect(Screen.width/4,Screen.height*4/8,Screen.width/2,Screen.height/8),"Keep Playing"))
+          {
+          count = 0;
+          Application.LoadLevel("gameover");
+}}}
