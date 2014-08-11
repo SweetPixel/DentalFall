645c5008a0705a34cbe431b6c967a590297a92a1
aliirz <dejavu87@gmail.com>
2014-08-11 14:40:52 +0500

    changed UNITY_ANDROID pragma

--- Pixel.Fall/Assets/GameoverHighScore.js
+++ Pixel.Fall/Assets/GameoverHighScore.js
@@ -1,55 +1,30 @@
 ﻿#pragma strict
 import UnityEngine;
-//#if UNITY_Android
-//import GooglePlayGames;
-//#endif
+import GooglePlayGames;
+
 
 var HighScore : GUIText;
 var explosion : AudioClip;
 
 function Start () {
 
-	// Authenticate and register a ProcessAuthentication callback
-    // This call needs to be made before we can proceed to other calls in the Social API
-    
-//    #if UNITY_Android
-    	// recommended for debugging:
-//    	try {
-//    		
-//    		PlayGamesPlatform.DebugLogEnabled = true;
-//    		// Activate the Google Play Games platform
-//    		PlayGamesPlatform.Activate();
-//    		Debug.Log("Seems Activated");
-//    	}
-//    	catch(UnityException)
-//    	{
-//    		Debug.Log("An Exception Occured");
-//    	}
-//    #endif
-    
-	
 
-
+	// Set label's text to the player's screen
 	HighScore.text = "(NEW) " + PlayerPrefs.GetInt("PlayerScore") + " / " + PlayerPrefs.GetInt("HighScore")+ " (HIGH) " ;
 	
 	audio.clip= explosion;
 	audio.Play();
 	
 	
-//	ILeaderboard leaderboard = UnityEngine.Social.CreateLeaderboard();
-//	leaderboard.id = "pfsandbox";
-	
-//	Social.ReportScore (PlayerPrefs.GetInt("HighScore"), leaderboard.id, success => {
-//			Debug.Log(success ? "Reported score successfully" : "Failed to report score");
-//	});
 
-			#if UNITY_Android
-				Social.ReportScore(PlayerPrefs.GetInt("HighScore"), "CgkIutDH9N0BEAIQAA",null);
-			#endif
+	
+	#if UNITY_ANDROID
+		Social.ReportScore(PlayerPrefs.GetInt("HighScore"), "CgkIutDH9N0BEAIQAA",null);
+	#endif
 			
-			#if UNITY_IPHONE
-				Social.ReportScore(PlayerPrefs.GetInt("HighScore"), "pfsandbox", null);
-			#endif
+	#if UNITY_IPHONE
+		Social.ReportScore(PlayerPrefs.GetInt("HighScore"), "pfsandbox", null);
+	#endif
 }
 
 function Update () {
