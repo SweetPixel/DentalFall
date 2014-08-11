a67c37e34c50bc952a0bb084429d16fdf0913077
ebtihaj-khan <ebtihaj_316@yahoo.com>
2014-08-11 19:52:11 +0500

    gameover scene UI
    changed UI gameover scene

--- Pixel.Fall/Assets/GameoverHighScore.js
+++ Pixel.Fall/Assets/GameoverHighScore.js
@@ -13,37 +13,37 @@ nowScore.fontSize=40;
 if(PlayerPrefs.GetInt("PlayerScore")<10 && PlayerPrefs.GetInt("HighScore")<10)
 {
 nowScore.text="    "+PlayerPrefs.GetInt("PlayerScore")+ " / "+ PlayerPrefs.GetInt("HighScore");
-HighScore.text = "     (NEW)                            (HIGH) " ;
+//HighScore.text = "     (NEW)                            (HIGH) " ;
 }
 
 
 else if((PlayerPrefs.GetInt("PlayerScore")<10) && (PlayerPrefs.GetInt("HighScore")>9) && (PlayerPrefs.GetInt("HighScore")<100))
 {
 nowScore.text="   " + PlayerPrefs.GetInt("PlayerScore")+ " / "+ PlayerPrefs.GetInt("HighScore");
-HighScore.text = "     (NEW)                            (HIGH) " ;
+//HighScore.text = "     (NEW)                            (HIGH) " ;
 }
 
 else if((PlayerPrefs.GetInt("PlayerScore")<10)  && (PlayerPrefs.GetInt("HighScore")>99) && (PlayerPrefs.GetInt("HighScore")<1000))
 {
 nowScore.text="  "+PlayerPrefs.GetInt("PlayerScore")+ " / "+ PlayerPrefs.GetInt("HighScore");
-HighScore.text = "    (NEW)                              (HIGH) " ;
+//HighScore.text = "    (NEW)                              (HIGH) " ;
 }
 
 else if((PlayerPrefs.GetInt("PlayerScore")<100) && (PlayerPrefs.GetInt("PlayerScore")>9) && (PlayerPrefs.GetInt("HighScore")>9) && (PlayerPrefs.GetInt("HighScore")<100))
 {
 nowScore.text="  "+PlayerPrefs.GetInt("PlayerScore")+ " / "+ PlayerPrefs.GetInt("HighScore");
-HighScore.text = "    (NEW)                              (HIGH) " ;
+//HighScore.text = "    (NEW)                              (HIGH) " ;
 }
 
 else if((PlayerPrefs.GetInt("PlayerScore")<100) && (PlayerPrefs.GetInt("PlayerScore")>9) && (PlayerPrefs.GetInt("HighScore")>99) && (PlayerPrefs.GetInt("HighScore")<1000))
 {
 nowScore.text=" "+PlayerPrefs.GetInt("PlayerScore")+ " / "+ PlayerPrefs.GetInt("HighScore");
-HighScore.text = " (NEW)                                   (HIGH) " ;
+//HighScore.text = " (NEW)                                   (HIGH) " ;
 }
 else if((PlayerPrefs.GetInt("PlayerScore")>99) && (PlayerPrefs.GetInt("PlayerScore")<1000) && (PlayerPrefs.GetInt("HighScore")>99) && (PlayerPrefs.GetInt("HighScore")<1000))
 {
 nowScore.text=PlayerPrefs.GetInt("PlayerScore")+ " / "+ PlayerPrefs.GetInt("HighScore");
-HighScore.text = "(NEW)                                      (HIGH) " ;
+//HighScore.text = "(NEW)                                      (HIGH) " ;
 }
 //audio.clip= explosion;
 //audio.Play();
