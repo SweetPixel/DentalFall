7aa1a14a4388d750e48c5096424475f2ef9d5bad
zahid--ali <zfz.zahid@yahoo.com>
2014-08-16 13:48:36 +0500

    don't know what going on

--- Pixel.Fall/Assets/gameovershare.cs
+++ Pixel.Fall/Assets/gameovershare.cs
@@ -1,25 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class gameovershare : MonoBehaviour {
-	public string decidelabel,closelabel;
+public class gameovershare : MonoBehaviour
+{
+		public string decidelabel, closelabel;
 	 
-	void Start(){
-		#if UNITY_ANDROID
-			DialogManager.Instance.SetLabel(decidelabel,null,closelabel);
-		#endif
-	}
-
-	void OnMouseDown () {
-		if (Application.internetReachability != NetworkReachability.NotReachable) {
-				
-						Application.LoadLevel ("share_ScoreFB");
-		} 
-		else {
-						Debug.Log ("ABC");
+		void Start ()
+		{
+				#if UNITY_ANDROID
+					DialogManager.Instance.SetLabel(decidelabel,null,closelabel);
+				#endif
 		}
-//			DialogManager.Instance.ShowSelectDialog("No Internet Connection",(bool result) =>{
-//
-//			});	
-	}
+
+		void OnMouseDown ()
+		{
+
+				if (Application.internetReachability != NetworkReachability.NotReachable) {
+					
+					Application.LoadLevel ("share_ScoreFB");
+				} 
+				else {
+					Debug.Log ("ABC");
+				}
+	//			DialogManager.Instance.ShowSelectDialog("No Internet Connection",(bool result) =>{
+	//
+	//			});	
+	
+
+			
+		}
 }
